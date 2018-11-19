15a7d002b7abc19307e99d5df9547b82787b332e
Anotinas <daviddaviddele@gmail.com>
2018-11-19 14:34:35 +0100

    Added general structure for client and two messages & 1 RPC for the proto

--- middleware/Client/Program.cs
+++ middleware/Client/Program.cs
@@ -11,9 +11,16 @@ namespace TrialClient
     {
         public static void Main(string[] args)
         {
-            Channel channel = new Channel("127.0.0.1", 23456, ChannelCredentials.Insecure);
+            //Testing();
+            //GetParsedCode()
+
+        }
+        private static void Testing(){
+            Console.WriteLine("Before Channel");
+            Channel channel = new Channel("192.168.43.224", 23456, ChannelCredentials.Insecure);
             var client = new Trial.Trial.TrialClient(channel);
-           String user = "David";
+            Console.WriteLine("After Channel");
+            String user = "Naief";
 
             var reply = client.MessageExamples(new HelloRequest { Name = user });
             Console.WriteLine("Greeting: " + reply.Message);
@@ -22,5 +29,21 @@ namespace TrialClient
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
+        private static void GetParsedCode(){
+
+            Console.WriteLine("Enter gitlab address of repository");
+            String gitlabURL = Console.ReadLine();
+            // Server request with gitlab address
+            Channel channel = new Channel("192.168.43.224", 23456, ChannelCredentials.Insecure);
+            var client = new Trial.Trial.TrialClient(channel);
+            var reply = client.MessageExamples(new HelloRequest { GitlabAdress = gitlabURL });
+            
+            // Handle the reply. Turn it into a Json file.
+
+
+            // Give the file to unity. 
+        }
     }
+        
+
 }
