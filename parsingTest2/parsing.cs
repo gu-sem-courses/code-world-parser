6df703d33736992f85cb7cba7f16937362c8b0b0
Naief <31688507+Naief@users.noreply.github.com>
2018-11-22 11:15:28 +0100

    Created as new folder called parsingTest2. It has the Visual Studio Code that runs perfectly

--- parsingTest2/parsing.cs
+++ parsingTest2/parsing.cs
@@ -33,7 +33,7 @@ class Parsing
         try
         {
            //String path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "producedJSON/test2.json");
-            String path = "../../producedJSON/test2.json";
+            String path = "producedJSON/test2.json";
             // serialize JSON to a string and then write string to a file
             File.WriteAllText(path, JsonConvert.SerializeObject(movie));
             // serialize JSON directly to a file
