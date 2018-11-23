04e6c3b66cab96b2f1b6e7e1c365a7d56ff852f7
Anotinas <daviddaviddele@gmail.com>
2018-11-23 15:40:11 +0100

    added a urlEncoder

--- middleware/GitGetter/Program.cs
+++ middleware/GitGetter/Program.cs
@@ -10,11 +10,15 @@ namespace GitFetcher{
             private static readonly HttpClient client = new HttpClient();
 
             public static void Main(){
-                gitTreeRetriever("");
+                //gitTreeRetriever("");
+                String url = "dit341/express-template";
+                String filelocation = "package.json";
+                
             }
 
-             public static String gitFileRetriever( String projectId, String repository, String branch , String filepath){ // This method is meant to take the id/filepath of a single file. 
-             String address = "www.gitlab.com/"+ projectId + "/"+ repository +"/raw/"+ branch +"/"+filepath;
+             public static String gitFileRetriever( String projectId_repository , String filepath){ // This method is meant to take the id/filepath of a single file. 
+             String address = "https://gitlab.com/"+ projectId_repository +"/raw/master/"+filepath;
+             Console.WriteLine(address);
 
             // String address = "https://gitlab.com/dit341/express-template/raw/master/package.json"; Example address
             
@@ -45,6 +49,7 @@ namespace GitFetcher{
             return "GitGetter\\FileStorer\\tempfile.txt";
         }
              private static void gitTreeRetriever(String projectId){
+                 // String address = "https://gitlab.com/api/v4/projects/" +"projectId" + "/repository/tree"
                  String address = "https://gitlab.com/api/v4/projects/dit341%2Fexpress-template/repository/tree";  //"https://gitlab.com/dit341/express-template/tree/master";
                 // The part where the request is actually made
                 try{
@@ -67,8 +72,32 @@ namespace GitFetcher{
                     Console.WriteLine(e);
                     //Write stuff incase the git repository was not found. 
                 }
+             }
+             public static String urlEncoder(String url){
+                 String encoded_url = "";
 
+                 char[] letters = url.ToCharArray();
+
+                 for(int i = 0; i< letters.Length; i++){
+                     if(letters[i] == '/'){
+                         String tempUrl = "";
+                         for(int x = 0; x < i; x++){
+                            tempUrl += letters[x];
+                         }
+                         tempUrl += "%2F";
+                         for(int y = i+1; y< letters.Length; y++){
+                             tempUrl += letters[y];
+                         }
+
+                         letters = tempUrl.ToCharArray();
+                     }
+                 
+                 }
+                 
+                 encoded_url = new string(letters);
+
+                 return encoded_url;
              }
         }
+}
         
-    }
