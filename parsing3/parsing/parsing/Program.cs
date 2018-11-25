b616498609b00c1f70be0c76e0d11b213ead5817
Naief <gusjobna@student.gu.se>
2018-11-25 15:09:13 +0100

    added for loops to exctract the class name. It works

--- parsing3/parsing/parsing/Program.cs
+++ parsing3/parsing/parsing/Program.cs
@@ -5,42 +5,87 @@ using System.Security.Permissions;
 using System.Xml;
 using System.Xml.Linq;
 
+
 [Serializable]
 
 
 class Program
 {
-    static void Main(string[] args)
+    protected static void Main(string[] args)
     {
-        /* //Read the XML file. 
+        /* //Read the XML file. This get me the entire XML file.
          XmlReader r = XmlReader.Create("parsing.xml");
          while (r.NodeType != XmlNodeType.Element)
              r.Read();
          XElement er = XElement.Load(r);
          Console.WriteLine(er);
          */
-
+        /*
         XmlReader r = XmlReader.Create("parsing.xml");
             while (r.Read())
+
          {
             if (r.NodeType == XmlNodeType.Element)
          {
-             Console.WriteLine(r.LocalName);
+             //Console.WriteLine(r.LocalName);
             }
-        }
+        }*/
 
 
-      /*  XmlDocument doc = new XmlDocument();
-        doc.Load("parsing.xml");
-        XmlElement root = doc.DocumentElement;
-        XmlNodeList nodes = root.SelectNodes("some_node"); // You can also use XPath here
-        foreach (XmlNode node in nodes)
+
+        XmlDocument xd = new XmlDocument();
+        xd.Load("parsing.xml");
+        //XmlNodeList nl = xd.SelectNodes("parsing.xml");
+        //XmlNode root = nl[0];
+
+        for (int i = 0; i < xd.ChildNodes.Count; i++)
         {
-            // use node variable here for your beeds
+
+            string name = xd.Name;
+            Console.WriteLine("SWAG.----------------1111");
+            Console.WriteLine(name);
+
+
         }
 
-        Console.WriteLine(nodes);
-        */
+        for (int j = 0; j < xd.ChildNodes.Count; j++)
+        {
+            //EITHER TRY TO FIX THIS OR USE THIS 
+            //https://support.microsoft.com/en-ca/help/307548/how-to-read-xml-from-a-file-by-using-visual-c
+
+            if (xd.HasAttribute("name"))
+            {
+                String name = xd.GetAttribute("name");
+                Console.WriteLine(name);
+            }
+
+        }
+
+        for (int k = 0; k < xd.ChildNodes.Count; k++)
+        {
+             if (xd.ChildNodes[k].HasChildNodes)
+             {
+                    Console.WriteLine("SWAG.---------------3333");
+                    Console.WriteLine(xd.ChildNodes[k].InnerXml);
+
+             }
+        }
+            //Console.WriteLine(xd.Chil);
+
+
+
+
+        /*  XmlDocument doc = new XmlDocument();
+          doc.Load("parsing.xml");
+          XmlElement root = doc.DocumentElement;
+          XmlNodeList nodes = root.SelectNodes("some_node"); // You can also use XPath here
+          foreach (XmlNode node in nodes)
+          {
+              // use node variable here for your beeds
+          }
+
+          Console.WriteLine(nodes);
+          */
 
 
         try
@@ -48,18 +93,18 @@ class Program
             //String path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "producedJSON/test2.json");
             String path = "../../producedJSON/test2.json";
             // serialize JSON to a string and then write string to a file
-            File.WriteAllText(path, JsonConvert.SerializeObject(r));
+            File.WriteAllText(path, JsonConvert.SerializeObject(xd));
             // serialize JSON directly to a file
             using (StreamWriter file = File.CreateText(path))
             {
                 JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(file, r);
+                serializer.Serialize(file, xd);
             }
         }
 
-        catch (Exception e)
+        catch (Exception u)
         {
-            Console.WriteLine(e.ToString());
+            Console.WriteLine(u.ToString());
         }
 
     }
