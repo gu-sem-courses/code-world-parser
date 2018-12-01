d5592a63124f5244b7cf39e412cde0830380ab3f
Trooper98 <oliver.g.ryan@gmail.com>
2018-12-01 18:34:11 +0100

    got the parser to read certain files

--- parser/parser/Parser.cs
+++ parser/parser/Parser.cs
@@ -9,67 +9,80 @@ class Program
 {
     protected static void Main(string[] args)
     {
-        //new xml object
-        XmlDocument xmlFile = new XmlDocument();
+        //Create an xml doc for existing file
+        XmlDocument originalXmlFile = new XmlDocument();
         //load project
-        xmlFile.Load("singleClass.xml");
-        //set the root ("unit" element)
-        XmlNode root = xmlFile.DocumentElement;
+        originalXmlFile.Load("singleClass.xml");
+        XmlNode root = originalXmlFile.DocumentElement;
 
-        if(root.HasChildNodes){
-            Console.WriteLine(root.Name);
-            Console.WriteLine("attributes:");
-            for (int i = 0; i < root.Attributes.Count; i++){
-                Console.WriteLine(root.Attributes[i].Value);
-            }
+        //create xml doc for the filtered xml file
+        XmlDocument doc = new XmlDocument();
+        XmlNode data = doc.CreateElement("data");
 
-            //add a namespace [this is necessary for the nodes to be selected]
-            XmlNamespaceManager namespaceManager = new XmlNamespaceManager(xmlFile.NameTable);
-            String srcMlNameSpace = "http://www.srcML.org/srcML/src";
-            namespaceManager.AddNamespace("", srcMlNameSpace);
+        //retrive the specific xml tags from old xml doc
+        XmlNodeList attrs = originalXmlFile.GetElementsByTagName("decl_stmt");
+        XmlNodeList meths = originalXmlFile.GetElementsByTagName("function"); 
 
-            //create an arrayList to store all elements with 'decl_stmt' tags
-            XmlNodeList elements= root.SelectNodes("/unit/class");
-            foreach (XmlNode element in elements)
-            {
-                Console.WriteLine(element.Value);
-                Console.WriteLine(element.Name);
-            }
+        //import elements to new xml doc
+        XmlNode attributes = doc.CreateElement("attributes");
+        XmlNode methods = doc.CreateElement("methods"); 
 
-        }// end of if-state controlling the child
+        attributes.AppendChild(importNodes(attrs, originalXmlFile, doc));
+        methods.AppendChild(importNodes(meths, originalXmlFile, doc));
 
-        /***** Will use later *****/
-        //try
-        //{
-        //    //String path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "producedJSON/test2.json");
-        //    String path = "../../../../assets/xml2json.json";
-        //    // serialize JSON to a string and then write string to a file
-        //    File.WriteAllText(path, JsonConvert.SerializeObject(xmlFile));
-        //    // serialize JSON directly to a file
-        //    using (StreamWriter file = File.CreateText(path))
-        //    {
-        //        JsonSerializer serializer = new JsonSerializer();
-        //        serializer.Serialize(file, xmlFile);
-        //    }
-        //}
+        //append elements to the new xml document
+        data.AppendChild(attributes);
+        data.AppendChild(methods);
 
-        //catch (Exception u)
-        //{
-        //    Console.WriteLine(u.ToString());
-        //}
+        XmlNode superclasses, subclasses, components, associations;
 
+        Console.WriteLine(data.InnerText);
     }
 
-    public static bool IsFertile(XmlNode node, String name){
-        if (node.HasChildNodes)
-        {
-            Console.WriteLine(name + " has childeren");
-            return true;
+
+    //----------------------------------------------------------------------------------------------------------------
+    public static XmlNode importNodes(XmlNodeList list, XmlDocument originDoc, XmlDocument destinationDoc){
+        //you cant really just reference a node from one doc to another so you need to import them first
+
+        //create a export node that stores import
+        XmlNode exportNode = destinationDoc.CreateElement("exportNode");
+
+        foreach(XmlNode node in list){
+            exportNode.AppendChild(destinationDoc.ImportNode(node,true));
         }
-        else
-        {
-            Console.WriteLine(name + " has no childeren");
-            return false;
-        }
+        return exportNode;
+    }
+
+    //WARNING
+    //the methods below are only reusable for OUR GROUP PROJECT (DIT3**)
+
+    public static XmlNode getAttributes(XmlNodeList list)
+    {
+        return null;
+    }
+
+    public static XmlNode getMethods(XmlNodeList list)
+    {
+        return null;
+    }
+
+    public static XmlNode getSubClasses(XmlNodeList list, XmlDocument originDoc, XmlDocument destinationDoc)
+    {
+        return null;
+    }
+
+    public static XmlNode getSuperClasses(XmlNodeList list, XmlDocument originDoc, XmlDocument destinationDoc)
+    {
+        return null;
+    }
+
+    public static XmlNode getComponents(XmlNodeList list, XmlDocument originDoc, XmlDocument destinationDoc)
+    {
+        return null;
+    }
+
+    public static XmlNode getAttributes(XmlNodeList list, XmlDocument originDoc, XmlDocument destinationDoc)
+    {
+        return null;
     }
 }
