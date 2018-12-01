a957840847b85aee67c4ad3d163964e95a1b47f2
Trooper98 <oliver.g.ryan@gmail.com>
2018-12-01 01:00:53 +0100

    I was able to manipulate the root node

--- assets/tests/sample/Employee.java
+++ assets/tests/sample/Employee.java
@@ -1,7 +1,7 @@
-package assignment3; 
+package assignment3;
 
-public class Employee implements Comparable<Employee>{
-	
+public class Employee implements Comparable<Employee> {
+
 	private final int EQUAL_RESULT = 0;
 	private final int INVERSE = -1;
 	private String name;
@@ -11,27 +11,24 @@ public class Employee implements Comparable<Employee>{
 	public static String choose;
 	public static String orderBy;
 
-
-	
 	public Employee(String name, String id, double grossSalary) {
-		this.name=name;
-		this.id=id;
-		this.grossSalary= grossSalary;
+		this.name = name;
+		this.id = id;
+		this.grossSalary = grossSalary;
 	}
-	
+
 	/*
-	 * this method is to keep the getGrossSalary() from changing, 
-	 * so all the changes will affect calculateSalary() not getGrossSalary().
+	 * this method is to keep the getGrossSalary() from changing, so all the changes
+	 * will affect calculateSalary() not getGrossSalary().
 	 */
-	public double calculateSalary() { 
+	public double calculateSalary() {
 		return this.grossSalary;
 	}
-	
+
 	public double getNetSalary() {
-		return this.grossSalary- (this.grossSalary*TAX);
+		return this.grossSalary - (this.grossSalary * TAX);
 	}
 
-	
 	@Override
 	public int compareTo(Employee newEmployee) {
 		int result = this.getName().compareToIgnoreCase(newEmployee.getName());
@@ -40,124 +37,95 @@ public class Employee implements Comparable<Employee>{
 		}
 		return result;
 	}
-	
 
-		
-		public String getName() {
-			return name;
-		}
-		
-		public void setName(String name) {
-			this.name = name;
-		}
-		
+	public String getName() {
+		return name;
+	}
 
-		public String getId() {
-			return id;
-		}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-		public double getGrossSalary() {
-			return grossSalary;
-		}
+	public String getId() {
+		return id;
+	}
 
-		public void setGrossSalary(double grossSalary) {
-			this.grossSalary=grossSalary;
-		}
-	
+	public double getGrossSalary() {
+		return grossSalary;
+	}
+
+	public void setGrossSalary(double grossSalary) {
+		this.grossSalary = grossSalary;
+	}
+
 	public String toString() {
-		return this.name+"'s gross Salary is of "+ this.grossSalary+" SEK per month. ";
+		return this.name + "'s gross Salary is of " + this.grossSalary + " SEK per month. ";
 	}
 	// CHALLENGE 1
-		/*@Override 
-		public int compareTo(Employee newEmployee) {
-			if (choose.equalsIgnoreCase("Name")) {
-				return compareByName(newEmployee);
-			} else if (choose.equalsIgnoreCase("Net Salary")) {
-				return compareByNetSalary(newEmployee);
-			} else {
-				return  ( this.getName().compareToIgnoreCase(newEmployee.getName()) )  ;
-			}
-
-		}
 	/*
-	 * we have two methods that will be called in the compareTo() 
-	 *  so depending on the String (choose) we will call either 
-	 *  compareByName() or compareByNetSalary().
-	 *  
-	 *   Regarding Challenge 1.
+	 * @Override public int compareTo(Employee newEmployee) { if
+	 * (choose.equalsIgnoreCase("Name")) { return compareByName(newEmployee); } else
+	 * if (choose.equalsIgnoreCase("Net Salary")) { return
+	 * compareByNetSalary(newEmployee); } else { return (
+	 * this.getName().compareToIgnoreCase(newEmployee.getName()) ) ; }
+	 * 
+	 * } /* we have two methods that will be called in the compareTo() so depending
+	 * on the String (choose) we will call either compareByName() or
+	 * compareByNetSalary().
+	 * 
+	 * Regarding Challenge 1.
 	 *
-		
-		public int compareByName (Employee newEmployee) {
-			int result;
-			result = this.getName().compareToIgnoreCase(newEmployee.getName());
-			if(result== EQUAL_RESULT) {
-				result= Double.compare(this.getNetSalary(), newEmployee.getNetSalary());
-				
-			}
-			return result;
-		}
-		
-			public int compareByNetSalary (Employee newEmployee) {
-				int result = Double.compare(this.getNetSalary(), newEmployee.getNetSalary());
-				if(result== EQUAL_RESULT) {
-					result = this.getName().compareToIgnoreCase(newEmployee.getName());	
-				}
-				return result;
-			}
-  */
-		
-	/*	CHALLENGE 2
-		@Override 
-		public int compareTo(Employee newEmployee) {
-			if (choose.equalsIgnoreCase("Name") && orderBy.equalsIgnoreCase("asc")) {
-				return compareByName(newEmployee);
-			}
-			else if (choose.equalsIgnoreCase("Name") && orderBy.equalsIgnoreCase("dasc")) {
-				return compareByNameDasc(newEmployee) ;
-			} else if (choose.equalsIgnoreCase("Net Salary") && orderBy.equalsIgnoreCase("asc")) {
-				return compareByNetSalary(newEmployee);
-			} else if (choose.equalsIgnoreCase("Net Salary") && orderBy.equalsIgnoreCase("dasc")) {
-				return compareByNetSalaryDasc(newEmployee);
-			}else {
-				return this.getName().compareToIgnoreCase(newEmployee.getName()) ;
-			}
+	 * 
+	 * public int compareByName (Employee newEmployee) { int result; result =
+	 * this.getName().compareToIgnoreCase(newEmployee.getName()); if(result==
+	 * EQUAL_RESULT) { result= Double.compare(this.getNetSalary(),
+	 * newEmployee.getNetSalary());
+	 * 
+	 * } return result; }
+	 * 
+	 * public int compareByNetSalary (Employee newEmployee) { int result =
+	 * Double.compare(this.getNetSalary(), newEmployee.getNetSalary()); if(result==
+	 * EQUAL_RESULT) { result =
+	 * this.getName().compareToIgnoreCase(newEmployee.getName()); } return result; }
+	 */
 
-		}
-		
-		 
-		public int compareByName(Employee newEmployee) {
-			int result;
-			result = this.getName().compareToIgnoreCase(newEmployee.getName());
-			if (result == EQUAL_RESULT) {
-				result = Double.compare(this.getNetSalary(), newEmployee.getNetSalary());
-			}
-			return result;
-		}
-		
-		public int compareByNetSalary(Employee newEmployee) {
-			int result = Double.compare(this.getNetSalary(), newEmployee.getNetSalary());
-			if (result == EQUAL_RESULT) {
-				result = this.getName().compareToIgnoreCase(newEmployee.getName());
-			}
-			return result;
-		}
-		
-		public int compareByNetSalaryDasc(Employee newEmployee) {
-			int result = ( Double.compare(this.getNetSalary(), newEmployee.getNetSalary()) * INVERSE );
-			if (result == EQUAL_RESULT) {
-				result = ( this.getName().compareToIgnoreCase(newEmployee.getName()) * INVERSE);
-			}
-			return result;
-		}
-		
-		public int compareByNameDasc(Employee newEmployee) {
-			int result;
-			result = ( this.getName().compareToIgnoreCase(newEmployee.getName()) * INVERSE);
-			if (result == EQUAL_RESULT) {
-				result = ( Double.compare(this.getNetSalary(), newEmployee.getNetSalary()) * INVERSE );
-			}
-			return result;
-		}
-	*/	
+	/*
+	 * CHALLENGE 2
+	 * 
+	 * @Override public int compareTo(Employee newEmployee) { if
+	 * (choose.equalsIgnoreCase("Name") && orderBy.equalsIgnoreCase("asc")) { return
+	 * compareByName(newEmployee); } else if (choose.equalsIgnoreCase("Name") &&
+	 * orderBy.equalsIgnoreCase("dasc")) { return compareByNameDasc(newEmployee) ; }
+	 * else if (choose.equalsIgnoreCase("Net Salary") &&
+	 * orderBy.equalsIgnoreCase("asc")) { return compareByNetSalary(newEmployee); }
+	 * else if (choose.equalsIgnoreCase("Net Salary") &&
+	 * orderBy.equalsIgnoreCase("dasc")) { return
+	 * compareByNetSalaryDasc(newEmployee); }else { return
+	 * this.getName().compareToIgnoreCase(newEmployee.getName()) ; }
+	 * 
+	 * }
+	 * 
+	 * 
+	 * public int compareByName(Employee newEmployee) { int result; result =
+	 * this.getName().compareToIgnoreCase(newEmployee.getName()); if (result ==
+	 * EQUAL_RESULT) { result = Double.compare(this.getNetSalary(),
+	 * newEmployee.getNetSalary()); } return result; }
+	 * 
+	 * public int compareByNetSalary(Employee newEmployee) { int result =
+	 * Double.compare(this.getNetSalary(), newEmployee.getNetSalary()); if (result
+	 * == EQUAL_RESULT) { result =
+	 * this.getName().compareToIgnoreCase(newEmployee.getName()); } return result; }
+	 * 
+	 * public int compareByNetSalaryDasc(Employee newEmployee) { int result = (
+	 * Double.compare(this.getNetSalary(), newEmployee.getNetSalary()) * INVERSE );
+	 * if (result == EQUAL_RESULT) { result = (
+	 * this.getName().compareToIgnoreCase(newEmployee.getName()) * INVERSE); }
+	 * return result; }
+	 * 
+	 * public int compareByNameDasc(Employee newEmployee) { int result; result = (
+	 * this.getName().compareToIgnoreCase(newEmployee.getName()) * INVERSE); if
+	 * (result == EQUAL_RESULT) { result = ( Double.compare(this.getNetSalary(),
+	 * newEmployee.getNetSalary()) * INVERSE ); } return result; }
+	 */
 
 }
--- parser/parser/Parser.cs
+++ parser/parser/Parser.cs
@@ -1,102 +1,75 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.IO;
-using System.Security.Permissions;
 using System.Xml;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Linq;
 
 [Serializable]
 
 class Program
 {
-    enum NodeTypes
-    {
-        HasChildren,
-        IsNode,
-        IsAttribute,
-        IsMethod
-    }
-
     protected static void Main(string[] args)
     {
-        var xmlFile = XDocument.Load("singleClass.xml");
-        Type type = xmlFile.GetType();
-        SpanXDocument(xmlFile.Root);
+        //new xml object
+        XmlDocument xmlFile = new XmlDocument();
+        //load project
+        xmlFile.Load("singleClass.xml");
+        //set the root ("unit" element)
+        XmlNode root = xmlFile.DocumentElement;
 
-        //get the class name, the attribute name and print it out
-        void SpanXDocument(XElement xmlTree)
-        {
-            XElement root = xmlTree;
-            XElement language, fileName, package, xClass;
-            NodeTypes nodeType;
-            foreach (XElement element in xmlTree.Elements())
-            {
-                //if (element.Descendants().Count() > 0)
-                //{
-                //    if(element.Descendants().Descendants().Count() > 0) 
-                //    {
-                //        nodeType = NodeTypes.HasChildren;
-                //    }
-                //    else
-                //    {
-                //        nodeType = NodeTypes.IsNode;
-                //    }
-                //}
-                //else
-                //{
-                //    nodeType = NodeTypes.IsAttribute;
-                //}
-                //switch (nodeType)
-                //{
-                //    case NodeTypes.HasChildren:
-                //        Console.WriteLine("Has Children Name : {0}",
-                //           element.Name.LocalName);
-                //        SpanXDocument(element);
-                //        break;
-                //    case NodeTypes.IsNode:
-                //        Console.WriteLine("Node Name : {0}",
-                //           element.Name.LocalName);
-                //        foreach (XElement elementNode in element.Elements())
-                //        {
-                //            Console.WriteLine("Node Attribute Name : {0} Value : {1}",
-                //               elementNode.Name.LocalName, elementNode.Value);
-                //        }
-                //        break;
-                //    case NodeTypes.IsAttribute:
-                //        Console.WriteLine("Attribute Name : {0} Value : {1}",
-                //           element.Name.LocalName, element.Value);
-                //        break;
-                //}
+        if(root.HasChildNodes){
+            Console.WriteLine(root.Name);
+            Console.WriteLine("attributes:");
+            for (int i = 0; i < root.Attributes.Count; i++){
+                Console.WriteLine(root.Attributes[i].Value);
             }
-        }
 
-        try
-        {
-            //String path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "producedJSON/test2.json");
-            String path = "../../../../assets/xml2json.json";
-            // serialize JSON to a string and then write string to a file
-            File.WriteAllText(path, JsonConvert.SerializeObject(xmlFile));
-            // serialize JSON directly to a file
-            using (StreamWriter file = File.CreateText(path))
+            //add a namespace [this is necessary for the nodes to be selected]
+            XmlNamespaceManager namespaceManager = new XmlNamespaceManager(xmlFile.NameTable);
+            String srcMlNameSpace = "http://www.srcML.org/srcML/src";
+            namespaceManager.AddNamespace("", srcMlNameSpace);
+
+            //create an arrayList to store all elements with 'decl_stmt' tags
+            XmlNodeList elements= root.SelectNodes("/unit/class");
+            foreach (XmlNode element in elements)
             {
-                JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(file, xmlFile);
+                Console.WriteLine(element.Value);
+                Console.WriteLine(element.Name);
             }
-        }
 
-        catch (Exception u)
-        {
-            Console.WriteLine(u.ToString());
-        }
+        }// end of if-state controlling the child
+
+        /***** Will use later *****/
+        //try
+        //{
+        //    //String path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "producedJSON/test2.json");
+        //    String path = "../../../../assets/xml2json.json";
+        //    // serialize JSON to a string and then write string to a file
+        //    File.WriteAllText(path, JsonConvert.SerializeObject(xmlFile));
+        //    // serialize JSON directly to a file
+        //    using (StreamWriter file = File.CreateText(path))
+        //    {
+        //        JsonSerializer serializer = new JsonSerializer();
+        //        serializer.Serialize(file, xmlFile);
+        //    }
+        //}
+
+        //catch (Exception u)
+        //{
+        //    Console.WriteLine(u.ToString());
+        //}
 
     }
 
-    public static XElement GetTreeChild()
-    {
-        XElement x = null;
-        return x;
+    public static bool IsFertile(XmlNode node, String name){
+        if (node.HasChildNodes)
+        {
+            Console.WriteLine(name + " has childeren");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine(name + " has no childeren");
+            return false;
+        }
     }
 }
