563c31e42831e4c5975e120596d25953f8c81cf9
Naief <31688507+Naief@users.noreply.github.com>
2018-12-12 05:54:26 +0100

    it keeps showing the same result no matter what. I am trying to only lookup the new operator

--- parser/parser/SrcReader.cs
+++ parser/parser/SrcReader.cs
@@ -252,8 +252,9 @@ namespace parser
         public void GetAssociations(XmlNode root, XmlNode classNode, XmlDocument xDoc, XmlNamespaceManager nsm)
         {
             String className = GetClassName(classNode, xDoc, nsm).InnerText;
-            XmlNodeList jClasses = xDoc.DocumentElement.SelectNodes("//src:class[.//src:decl_stmt//src:decl/src:type//src:name = \"" + className + "\" + //src:init = src:expr//src:operator[.= 'new']/src:call/src:name/src:name/src:argument_list]", nsm); 
+            //XmlNodeList jClasses = xDoc.DocumentElement.SelectNodes("//src:class[.//src:decl_stmt//src:decl/src:type//src:name = \"" + className + "\" + //src:init = src:expr//src:operator[.= 'new']/src:call/src:name/src:name/src:argument_list]", nsm); 
 
+            XmlNodeList jClasses = xDoc.DocumentElement.SelectNodes("//src:class[//src:decl_stmt//src:decl//src:operator[.= 'new']]", nsm);        
             foreach (XmlNode jClass in jClasses)
             {
                 
