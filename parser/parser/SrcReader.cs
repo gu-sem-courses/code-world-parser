b188c9ea6ef16f8d7b969b2c62beea7f644c695c
Naief <31688507+Naief@users.noreply.github.com>
2018-12-16 17:57:24 +0100

    only gotta filter out the un-necessary stuff

--- parser/parser/SrcReader.cs
+++ parser/parser/SrcReader.cs
@@ -251,13 +251,13 @@ namespace parser
 
         public void GetAssociations(XmlNode root, XmlNode classNode, XmlDocument xDoc, XmlNamespaceManager nsm)
         {
-            //String className = GetClassName(classNode, xDoc, nsm).InnerText;
+            String className = GetClassName(classNode, xDoc, nsm).InnerText;
             //XmlNodeList jClasses = xDoc.DocumentElement.SelectNodes("//src:class[.//src:decl_stmt//src:decl/src:type//src:name //src:init = src:expr/src:operator[.= 'new']/src:call/src:name/src:name/src:argument_list[.= '()']]", nsm);
             //ask oli about the query
             // XmlNodeList jClasses = xDoc.DocumentElement.SelectNodes("//src:class[//src:decl//src:operator[.= 'new']]", nsm);  
 
             //Dunction >> Component >> association
-            XmlNodeList jClasses = xDoc.DocumentElement.SelectNodes("//src:class//src:function//src:decl_stmt//src:type//src:name", nsm); 
+            XmlNodeList jClasses = xDoc.DocumentElement.SelectNodes("//src:class//src:function//src:decl_stmt//src:decl//src:type//src:name =  \"" + className + "\" ", nsm); 
             //XmlNodeList jClasses = xDoc.DocumentElement.SelectNodes("//src:class[.//src:decl_stmt//src:decl/src:type//src:name = \"" + className + "\"]", nsm);
             //XmlNode aClasses = xDoc.DocumentElement.SelectSingleNode("//src:class[.//src:decl_stmt//src:decl/src:type//src:name //src:init = src:expr/src:operator[.= 'new']/src:call/src:name/src:name/src:argument_list[.= '()']]", nsm);
             //XmlNodeList classList = xDoc.DocumentElement.SelectNodes("//src:class[src:specifier[.!='abstract']]", nsm);
