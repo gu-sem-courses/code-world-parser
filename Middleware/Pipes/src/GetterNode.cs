a1733616b7c8b1f7fae54c206b15253ef5edf2b6
Jorelsin <jorelsin@gmail.com>
2019-01-13 12:13:40 +0100

    refactoring of code

---
+++ Middleware/Pipes/src/GetterNode.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+using Services;
+using System.IO;
+using Grpc.Core;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace Pipes
+{ //Wierd name, but the class it extends from is a class generted by the proto file/files
+    public class GetterService : Services.GameLog.GameLogBase
+    {
+        
+        //this is the function that we have the client process call, 
+        //the parameters type and the name and what it returns are set in the proto generated files Trial.cs & TrialGrpc.cs
+        public override Task<JsonReply> MainInteraction(ParsingRequest request, ServerCallContext context)
+        {
+            
+            Console.WriteLine("Being called by " + context.Host.ToString());
+            GetProject(request);
+            string filepath = "../../../../../dit355/globalAssets/outbox/xml2json.json";
+            string result = string.Empty;
+            using (StreamReader r = new StreamReader(filepath))
+            {
+                var json = r.ReadToEnd();
+                JObject jsonobj = JObject.Parse(json);
+                result = jsonobj.ToString();
+            }
+            return Task.FromResult(new JsonReply { File = result });
+        }
+
+        // This function starts a the GitGetter process and passes along the request message 
+        // from the client proccess as a argument while starting the process
+        public void GetProject(ParsingRequest req)
+        {
+            string PathP = System.AppDomain.CurrentDomain.BaseDirectory + "../../../GitGetter/bin/Debug/Gitgetter.exe";
+            Console.WriteLine(PathP);
+            Process GitGetter = new Process();
+            try
+            {
+                //so it know where to find the file it should use to start the proccess
+                //if no actuall file is specified it will just open the specified folder
+                GitGetter.StartInfo.FileName = PathP;
+                // What arguments the file will take when it starts
+                Console.WriteLine(req.Address.ToString());
+                GitGetter.StartInfo.Arguments = req.Address.ToString();
+                GitGetter.Start();
+                GitGetter.WaitForExit();
+                GetterNode.ClietRequest();
+              
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+    // This class contain the "Static void Main" in wich it starts the "Server" and gives it a set of services and a port to listen to.
+    public class GetterNode
+    {
+        //the host IP, 0.0.0.0 (127.0.0.1 seems to work just as well) means it hosts it locally, 
+        //not sure if you can change to another computers IP
+        // The Port 23456 is the specific port the proccess will listen on for requests. 
+        // the port needs to be the same on the Server and Client proccesses.
+       
+        const int Port = 23456;
+       
+        public static void Start()
+        {
+            Console.WriteLine("Please type in your IP");
+            string Host = Console.ReadLine();
+            // string Host = "127.0.0.1";
+            // var Host = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
+            Server server = new Server
+            {
+                //the services or functions that the Server can peform, I guess we can add more if we need to.
+                Services = { Services.GameLog.BindService(new GetterService()) },
+                Ports = { { new ServerPort(Host, Port, ServerCredentials.Insecure) } }
+                
+            };
+
+            server.Start();
+
+            // This is just here so the program doesnt just shutdown when it gets done, could be nice to add 
+            // quality of life messages here but can´t think of any right now
+            
+            Console.WriteLine("Server listening on port " + Port + " and Domain is " +  Host);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+            server.ShutdownAsync().Wait();
+        }
+
+        public static void ClietRequest()
+        {
+            string filepathXML = "../../../../../dit355/globalAssets/inbox/srcML.xml";
+            string filepathJSON = "../../../../../dit355/globalAssets/outbox/xml2json.json";
+
+            // if you just want it to run localy on your computer comment out the line below
+            // IP = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().HostName;
+
+            Console.WriteLine("Give us the IP of the Node your trying to reach");
+            string IP = Console.ReadLine();
+            // string IP = "127.0.0.1";
+            int Port = 23455;
+            Channel channel = new Channel(IP, Port, ChannelCredentials.Insecure);
+            var client = new Services.GameLog.GameLogClient(channel);
+            string result = string.Empty;
+            XDocument file = XDocument.Load(filepathXML);
+            result = file.ToString();
+            // Console.WriteLine(result);
+            
+            var json = client.MainInteraction(new ParsingRequest { Address = WebUtility.HtmlEncode(result)});
+            System.IO.File.WriteAllText(filepathJSON, json.File);
+            channel.ShutdownAsync().Wait();
+        }
+    }
+
+    
+}
---
+++ Middleware/Pipes/src/UnityNode.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Services;
+
+namespace Pipes
+{
+    class UnityNode
+    {
+        public static void Start(string[] args)
+        {
+            //Environment.SetEnvironmentVariable("GRPC_TRACE", "api");
+            //Environment.SetEnvironmentVariable("GRPC_VERBOSITY", "debug");
+            //Grpc.Core.GrpcEnvironment.SetLogger(new Grpc.Core.Logging.ConsoleLogger());
+            // initiate variables 
+            string repository;
+            string UserName;
+            string ProjectName;
+            string filepath;
+            string page;
+            string filetype = ".json";
+            Console.WriteLine("Give us the IP of the Node your trying to reach");
+            string IP = Console.ReadLine();
+            // string IP = "127.0.0.1";
+            // Console.WriteLine(IP);
+            //string IP = "129.16.31.63";
+            // string IP = "10.0.97.223";
+            // string IP = "LAPTOP-F7LVEQ0B";
+
+
+            int Port = 23456;
+
+            // Checks if the we succesfully got the correct amount of arguments
+            // otherwise we set it to a pre-know project
+            if (args.Length >= 3)
+            {
+                UserName = args[0];
+                ProjectName = args[1];
+                page = args[2];
+
+            }
+            else if (args.Length == 2)
+            {
+                UserName = args[0];
+                ProjectName = args[1];
+                page = "t";
+            }
+            else
+            {
+                // this should be changed as it reference a none java project
+                UserName = "dit341";
+                ProjectName = "express-template";
+                page = "t";
+            }
+
+            //This could already be done before we give the parameters 
+            //but doing it this way makes it easier to name the json file based on the 
+            // project that you are getting
+            repository = UserName + "/" + ProjectName + " " + page;
+
+            //This will set up the channel we will use to communicate with the "Server" process
+            // the first value refers to the IP of the PC that is running the Server process, the second is what port to send it to
+            // and the third is ChannelCredentials that have to match the one of the Server
+            Channel channel = new Channel(IP,Port,ChannelCredentials.Insecure);
+
+            // This makes a new client variable referencing the Trial and TrialGrpc files
+            var client = new Services.GameLog.GameLogClient(channel);
+
+            //This makes a new json variable and provides a request message, the return message will then be saved in the json var
+            var json = client.MainInteraction(new ParsingRequest { Address = repository });
+
+            //sets the variable so we always make the file in the same place no matter the PC that runs it
+            //the filetype variable at the end decides what kind of file we will make.
+            filepath = System.AppDomain.CurrentDomain.BaseDirectory + "../Project" + filetype;
+
+            //This will write the file on the computer
+            System.IO.File.WriteAllText(filepath, json.File);
+            channel.ShutdownAsync().Wait();
+        }
+    }
+}
